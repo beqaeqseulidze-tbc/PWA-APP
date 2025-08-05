28292fa690a7b93d85b6761995bab4b116c0f02c
Beka Ekseulidze <BEkseulidze@Tbcbank.com.ge>
2025-08-05 10:21:18 +0400

    Auto-generated commit

---
+++ custom-sw.js
@@ -0,0 +1,20 @@
+// ეს ხაზი აუცილებელია, რომ Angular-ის სტანდარტული Service Worker-ის ფუნქციონალი (ქეშირება და ა.შ.) 
+// არ დაიკარგოს.
+importScripts('./ngsw-worker.js');
+
+// ვუსმენთ notification-ის მოვლენებს რათა დავჰენდლოთ ნოტიფიკაციაზე იუზერის ქლიქი
+self.addEventListener('notificationclick', (event) => {
+
+  const notificationData = event.notification.data;
+
+  // ვამოწმებთ, თუ არსებობს URL ჩვენს data ობიექტში
+  if (notificationData && notificationData.url) {
+    // ვხურავთ ნოტიფიკაციას
+    event.notification.close();
+    
+    // ვეუბნებით ბრაუზერს, რომ დაელოდოს ახალი ფანჯრის გახსნას
+    event.waitUntil(
+      clients.openWindow(notificationData.url)
+    );
+  }
+});
